5a3d17e4a9910cda7beaac757386f237825277c8
K8MY <k8my@pge.com>
2020-12-08 16:01:51 -0800

    add new test

--- PSPSProject/src/Tests/test_psps_ui_defaultmanagement_positive_e2e.py
+++ PSPSProject/src/Tests/test_psps_ui_defaultmanagement_positive_e2e.py
@@ -1,16 +1,30 @@
 import os
+import datetime
 import time
 
 import pytest
+import pandas as pd
+import boto3
 
 from PSPSProject.src.Pages.DefaultManagement import DefaultManagement
 from PSPSProject.src.Pages.HomePage import HomePage
 from PSPSProject.src.Repository.uilocators import locators
 from PSPSProject.src.Repository.statictext import textMessage
+from PSPSProject.src.Repository.dbqueries import queries
 from PSPSProject.src.ReusableFunctions.baseclass import BaseClass, exceptionRowCount
-from PSPSProject.src.ReusableFunctions.commonfunctions import logfilepath, deleteFiles, readData, getCSVrowCount
+from PSPSProject.src.ReusableFunctions.databasefunctions import *
+from PSPSProject.src.ReusableFunctions.awsfunctions import *
+from PSPSProject.src.ReusableFunctions.commonfunctions import logfilepath, deleteFiles, readData, getCSVrowCount, getCurrentTime
 from PSPSProject.src.ReusableFunctions.uiactions import UI_Element_Actions
 from PSPSProject.src.Tests.conftest import downloadsfolder, testDatafilePath, testDatafolderPath
+from PSPSProject.src.Tests.conftest import downloadsfolder, testDatafilePath, s3config, dirctorypath
+
+from PSPSProject.src.Repository.dbqueries import queries
+from PSPSProject.src.ReusableFunctions.awsfunctions import download_file_from_S3, download_dir_from_S3
+from PSPSProject.src.ReusableFunctions.baseclass import BaseClass, exceptionRowCount
+from PSPSProject.src.ReusableFunctions.commonfunctions import logfilepath, deleteFiles, downloadsfolderPath, readData, \
+    deleteFolder, create_folder
+from PSPSProject.src.ReusableFunctions.databasefunctions import queryresults_get_alldata
 
 VAR_TESTCASENAME = os.path.basename(__file__)
 global VAR_COUNT, VAR_TESTENV
@@ -40,6 +54,22 @@ class TestDefaultManagementPositive(BaseClass):
             homepage.SignOn()
             log.info("Successfully entered user id & password:")
             print("Successfully entered user id & password:")
+
+        # filename = "s3://psps-datastore-dev/reports/defaultmanagement/defaultmanagement_circuit_12_04_2020_11_54_12.parquet"
+        # var_tp_uid = "169"
+
+        # s3 = boto3.client('s3')
+        # s3_resource = boto3.resource("s3")
+        # s3_bucketname = s3config()['datastorebucketname']
+        # #BUCKET_PATH = s3config()['tpbucketpath']
+        # #path = BUCKET_PATH + filename
+        # path = s3_bucketname + filename
+        # profilename = s3config()['profile_name']
+        # local_folder = downloadsfolderPath + "circuits_" + str(var_tp_uid)
+        # deleteFolder(local_folder)
+        # var_res = download_dir_from_S3(path, s3_bucketname, profilename, local_folder)
+        # print (var_res)
+        # log.info("Downloaded circuits parquet file from S3")
 
         homepage.navigate_defaultManagement()
         uielements.Click(locators.dm_uplaodfile)
@@ -73,7 +103,6 @@ class TestDefaultManagementPositive(BaseClass):
         var_flag = uielements.iselementEnabled(locators.dm_save_btn)
         if var_flag:
             log.info("Save button is enabled after the valid circuit file uploaded")
-
             uielements.Click(locators.dm_save_btn)
             log.info("Click on Save button")
             while True:
@@ -97,6 +126,20 @@ class TestDefaultManagementPositive(BaseClass):
             log.error("Save button is not enabled after the valid circuit file uploaded")
             final_assert.append(False)
 
+        # Validate database for S3 file path
+        var_s3_path_db = queryresults_get_one(queries.get_activetablename)
+        log.info("Get S3 file path from DB: " + str(var_s3_path_db))
+
+        var_today = str(datetime.date.today())
+        var_year = var_today.split('-')[0]
+        var_month = var_today.split('-')[1]
+        var_day = var_today.split('-')[2]
+        var_date_validate = var_month + "_" + var_day + "_" + var_year
+
+        # Validate that currect date present in the S3 path
+        if str(var_date_validate) in str(var_s3_path_db):
+            log.info("Validate S3 path in database passed: " + str(var_s3_path_db))
+
         # Verify message retained in the Default management screen on navigating to other pages and returning back
         homepage.navigate_eventManagement()
         homepage.navigate_defaultManagement()
@@ -106,8 +149,6 @@ class TestDefaultManagementPositive(BaseClass):
         else:
             log.error("Verify 'Default devices data uploaded successfully' message retained page on navigating to other pages and returning failed")
             final_assert.append(False)
-
-
 
         log.info("*************AUTOMATION EXECUTION COMPLETED*************")
 
