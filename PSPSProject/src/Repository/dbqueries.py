1d53436843678e4b633b782a6b43c18682d59b54
K8MY <k8my@pge.com>
2020-12-08 16:00:49 -0800

    add new test

--- PSPSProject/src/Repository/dbqueries.py
+++ PSPSProject/src/Repository/dbqueries.py
@@ -1,3 +1,4 @@
 class queries:
     # Version Info table
     get_versionid = "select * from psps.versioninfo where versionname = '%s'"
+    get_activetablename = "select activetablename from psps.table_info where componentname = 's3-defaultmanagement-circuits'"
