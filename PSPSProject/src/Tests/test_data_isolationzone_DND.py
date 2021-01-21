8380cf0b8ecabe8133e9ce8875dc0733d9101e98
d2nd <d2nd@pge.com>
2021-01-20 21:42:48 -0800

    updated

--- PSPSProject/src/Tests/test_data_isolationzone_DND.py
+++ PSPSProject/src/Tests/test_data_isolationzone_DND.py
@@ -85,26 +85,26 @@ class TestIsolationZone(BaseClass):
             else:
                 var_tp_array.append(timeplace)
 
-        # Get the latest feeder devices table filepath from db
-        get_feederNetwork_devicestable_db = queries.get_activetablename % 's3-feedernetworktrace-devices'
-        lst_table_details1 = queryresults_fetchone(get_feederNetwork_devicestable_db)
-        filename_feederdevicestable = lst_table_details1
-        log.info("Filename for Feeder Network - devices active table is : " + str(filename_feederdevicestable))
-        log.info("-----------------------------------------------------------------------------------------------")
-        feederdevicestablefilename = filename_feederdevicestable.split(s3config()['envpath'])[-1]
-        print(feederdevicestablefilename)
-
-        # Download feederdevices parquet file from S3- PSPSDataSync folder
-        s3 = boto3.client('s3')
-        s3_resource = boto3.resource("s3")
-        s3_bucketname = s3config()['pspsdatabucketname']
-        feederdevicesBUCKET_PATH = feederdevicestablefilename
-        profilename = s3config()['profile_name']
-        feederdevices = downloadsfolderPath + "\\feederNetwork_devices"
+        # # Get the latest feeder devices table filepath from db
+        # get_feederNetwork_devicestable_db = queries.get_activetablename % 's3-feedernetworktrace-devices'
+        # lst_table_details1 = queryresults_fetchone(get_feederNetwork_devicestable_db)
+        # filename_feederdevicestable = lst_table_details1
+        # log.info("Filename for Feeder Network - devices active table is : " + str(filename_feederdevicestable))
+        # log.info("-----------------------------------------------------------------------------------------------")
+        # feederdevicestablefilename = filename_feederdevicestable.split(s3config()['envpath'])[-1]
+        # print(feederdevicestablefilename)
+        #
+        # # Download feederdevices parquet file from S3- PSPSDataSync folder
+        # s3 = boto3.client('s3')
+        # s3_resource = boto3.resource("s3")
+        # s3_bucketname = s3config()['pspsdatabucketname']
+        # feederdevicesBUCKET_PATH = feederdevicestablefilename
+        # profilename = s3config()['profile_name']
+        # feederdevices = downloadsfolderPath + "\\feederNetwork_devices"
         # deleteFolder(feederdevices)
         # create_folder(feederdevices)
         # download_dir_from_S3(feederdevicesBUCKET_PATH, s3_bucketname, profilename, feederdevices)
-        log.info("Downloaded feederNetwork_devices parquet file from S3")
+        # log.info("Downloaded feederNetwork_devices parquet file from S3")
 
         # Get the latest ssd-isolationzone table filepath from db
         get_ssd_isolationzonetable_db = queries.get_activetablename % 's3-ssd-isolationzone'
@@ -122,9 +122,9 @@ class TestIsolationZone(BaseClass):
         ssd_isolationzoneBUCKET_PATH = ssd_isolationzonetablefilename
         profilename = s3config()['profile_name']
         ssd_isolationzone = downloadsfolderPath + "\\ssd_isolationzone"
-        # deleteFolder(ssd_isolationzone)
-        # create_folder(ssd_isolationzone)
-        # download_dir_from_S3(ssd_isolationzoneBUCKET_PATH, s3_bucketname, profilename, ssd_isolationzone)
+        deleteFolder(ssd_isolationzone)
+        create_folder(ssd_isolationzone)
+        download_dir_from_S3(ssd_isolationzoneBUCKET_PATH, s3_bucketname, profilename, ssd_isolationzone)
         log.info("Downloaded ssd_isolationzone parquet file from S3")
 
         # Get Timeplace UID and Timeplace ID for the required timeplace
@@ -204,6 +204,7 @@ class TestIsolationZone(BaseClass):
             df_ssd_isolationzonedataloc.coalesce(1).write.option("header", "true").format("csv").mode("overwrite").save(
                 tempfolder)
 
+
             # df_tempcircuits = spark.sql("""Select circuitinfo_uid, timeplace_foreignkey, circuitid, circuitname,
             # substationname, transmissionimpact, division, circuitid || '-' || source_isolation_device ||
             # '-' || source_isolation_device_type as isolationzonename, source_min_branch, source_max_branch,
@@ -216,30 +217,13 @@ class TestIsolationZone(BaseClass):
             # df_tempcircuits.coalesce(1).write.option("header", "true").format("csv").mode("overwrite").save(
             #     tempfolder)
 
-            # Read FeederNetwork_devices table
-            # feedertransformerdataloc = downloadsfolderPath + "\\feederNetwork_transformer"
-            df_feederdevicesdataloc = spark.read.parquet(feederdevices + '/' + feederdevicesBUCKET_PATH)
-            df_feederdevicesdataloc.createOrReplaceTempView("feederdevices")
-            tempfolder = downloadsfolderPath + '\\S3_feederdevicesdata'
-            df_feederdevicesdataloc.coalesce(1).write.option("header", "true").format("csv").mode("overwrite").save(
-                tempfolder)
-
-            df_isolationzones = spark.sql("""SELECT '' as isolationzone_uid, '' as concat_circuitinfo_uid,
-            c.circuitinfo_uid, c.timeplace_foreignkey, c.circuitid, c.circuitname,
-            c.source_isolation_device, c.source_isolation_device_type, c.additional_isolation_device, 
-            c.additional_isolation_device_type, f.feederid || '-' || f.OPERATINGNUMBER || '-' || f.ASSETTYPE 
-            as isolationzone from timeplace_circuits as c inner join feederdevices as f on
-            LPAD(f.FEEDERID,9,'0') = LPAD(c.circuitid,9,'0')
-            and f.min_branch >= c.source_min_branch
-            and f.max_branch <= c.source_max_branch 
-            and f.order_num <= c.source_order_num 
-            and f.treelevel >= c.source_treelevel 
-            and f.ASSETTYPE in ('Breaker','DPD','Fuse','Switch') 
-            and f.DEVICESTATUS = 'close'""")
-            df_isolationzones.createOrReplaceTempView("isolationzones")
-            tempfolder = downloadsfolderPath + '\\isolationzones_feederdevices'
-            df_isolationzones.coalesce(1).write.option("header", "true").format("csv").mode("overwrite").save(
-                tempfolder)
+            # # Read FeederNetwork_devices table
+            # # feedertransformerdataloc = downloadsfolderPath + "\\feederNetwork_transformer"
+            # df_feederdevicesdataloc = spark.read.parquet(feederdevices + '/' + feederdevicesBUCKET_PATH)
+            # df_feederdevicesdataloc.createOrReplaceTempView("feederdevices")
+            # tempfolder = downloadsfolderPath + '\\S3_feederdevicesdata'
+            # df_feederdevicesdataloc.coalesce(1).write.option("header", "true").format("csv").mode("overwrite").save(
+            #     tempfolder)
 
             df_tempisozonedata = spark.sql("""SELECT '' as isolationzone_uid, '' as concat_circuitinfo_uid,
             c.circuitinfo_uid, c.timeplace_foreignkey, iz.circuitid as circuitId, iz.circuitname,
@@ -247,85 +231,91 @@ class TestIsolationZone(BaseClass):
             c.additional_isolation_device, c.additional_isolation_device_type, iz.isolationzone as isolationzonename, '' as flag,
             '' as transmissionimpact, '' as restorationzonename, iz.tier2ohmiles, iz.tier3ohmiles, iz.totalohmiles, iz.tier2ugmiles,
             iz.tier3ugmiles, iz.totalugmiles, iz.tier2ohpoles, iz.tier3ohpoles, iz.totalpoles, iz.shape, '' as impacttype from
-            isolationzones as c LEFT JOIN ssd_isolationzone iz
-            on iz.isolationzone = c.isolationzone""")
+            timeplace_circuits as c inner join ssd_isolationzone iz
+            on LPAD(iz.circuitid,9,'0') = LPAD(c.circuitid,9,'0')
+            AND iz.TREELEVEL >= c.source_treelevel
+            AND iz.ORDER_NUM <= c.source_order_num
+            AND iz.MAX_BRANCH <= c.source_max_branch
+            AND iz.MIN_BRANCH >= c.source_min_branch""")
             df_tempisozonedata.createOrReplaceTempView("tempisolationzonedata")
             tempfolder = downloadsfolderPath + '\\df_tempisolationzonedata'
             df_tempisozonedata.coalesce(1).write.option("header", "true").format("csv").mode("overwrite").save(
                 tempfolder)
 
-            # # Store df_tempisolationzonedata csv to dataframe
-            # tempfolder = downloadsfolderPath + '\\df_tempisolationzonedata'
-            # isolationzonecircuits = os.listdir(tempfolder)
-            # for file in isolationzonecircuits:
-            #     if file.endswith('csv'):
-            #         break
-            # finalisolationzonecircuitscsv = downloadsfolderPath + '\\df_tempisolationzonedata' + '/' + file
-            # finalisolationzonecircuits_actual = pd.read_csv(finalisolationzonecircuitscsv)
-            #
-            # # Store devfile_isolationzonecircuits csv to dataframe
-            # tempisozonedata = downloadsfolderPath + '\\devfile_isolationzonecircuits'
-            # isolationzonecircuitscsv_expected = os.listdir(tempisozonedata)
-            # for file1 in isolationzonecircuitscsv_expected:
-            #     if file1.endswith('csv'):
-            #         break
-            # finalisolationzonecircuitscsv_expected = downloadsfolderPath + '\\devfile_isolationzonecircuits' + '/' + file1
-            # finalisolationzonecircuits_expected = pd.read_csv(finalisolationzonecircuitscsv_expected)
-            #
-            # finalisolationzonecircuits_actual = finalisolationzonecircuits_actual.drop_duplicates(
-            #     subset=['circuitId', 'isolationzonename', 'tier2ohmiles', 'tier3ohmiles', 'totalohmiles',
-            #             'tier2ugmiles', 'tier3ugmiles', 'totalugmiles', 'tier2ohpoles', 'tier3ohpoles', 'totalpoles'],
-            #     keep='first')
-            # finalizcircuitsfolder = downloadsfolderPath + "\\Autofile_isolationzonecircuits"
-            # deleteFolder(finalizcircuitsfolder)
-            # create_folder(finalizcircuitsfolder)
-            # finalisolationzonecircuits_actual.to_csv(finalizcircuitsfolder + '/finalisozone_actual.csv')
-            #
-            # actualizcount = finalisolationzonecircuits_actual.shape[0]
-            # log.info("Automation file Count of isolation zone records is: " + str(actualizcount))
-            # expectedizcount = finalisolationzonecircuits_expected.shape[0]
-            # log.info("Dev file Count of isolation zone records is: " + str(expectedizcount))
-            # if str(actualizcount) == str(expectedizcount):
-            #     log.info(
-            #         "Total isolation zone records count matched between Actual and Expected report and count is: " + str(
-            #             actualizcount))
-            #     df1 = pd.merge(finalisolationzonecircuits_expected, finalisolationzonecircuits_actual,
-            #                    on=['circuitId', 'isolationzonename', 'tier2ohmiles', 'tier3ohmiles', 'totalohmiles',
-            #                        'tier2ugmiles', 'tier3ugmiles', 'totalugmiles', 'tier2ohpoles', 'tier3ohpoles',
-            #                        'totalpoles'],
-            #                    how='outer', indicator=True)
-            #     df1 = df1[df1['_merge'] != 'both']
-            #     if len(df1) == 0:
-            #         log.info("All the isolationzones matched")
-            #     else:
-            #         mismatchisolationzones = downloadsfolderPath + "\\mismatchisolationzones"
-            #         deleteFolder(mismatchisolationzones)
-            #         create_folder(mismatchisolationzones)
-            #         df1.to_csv(mismatchisolationzones + '/mismatchisolationzones.csv')
-            #         log.error(
-            #             "All the isolationzones not matched and mismatched isolationzones are: " + str(df1))
-            #         final_assert.append(False)
-            # else:
-            #     log.error("Total isolationzones count not matched between Actual and Expected report")
-            #     final_assert.append(False)
-            #     df1 = pd.merge(finalisolationzonecircuits_expected, finalisolationzonecircuits_actual,
-            #                    on=['circuitId', 'isolationzonename', 'tier2ohmiles', 'tier3ohmiles', 'totalohmiles',
-            #                        'tier2ugmiles', 'tier3ugmiles', 'totalugmiles', 'tier2ohpoles', 'tier3ohpoles',
-            #                        'totalpoles'],
-            #                    how='outer', indicator=True)
-            #     df1 = df1[df1['_merge'] != 'both']
-            #     if len(df1) == 0:
-            #         log.info("All the isolationzones matched")
-            #     else:
-            #         mismatchisolationzones = downloadsfolderPath + "\\mismatchisolationzones"
-            #         deleteFolder(mismatchisolationzones)
-            #         create_folder(mismatchisolationzones)
-            #         df1.to_csv(mismatchisolationzones + '/mismatchisolationzones.csv')
-            #         log.error(
-            #             "All the isolationzones not matched and mismatched isolationzones are: " + str(df1))
-            #         final_assert.append(False)
+            # Store df_tempisolationzonedata csv to dataframe
+            tempfolder = downloadsfolderPath + '\\df_tempisolationzonedata'
+            isolationzonecircuits = os.listdir(tempfolder)
+            for file in isolationzonecircuits:
+                if file.endswith('csv'):
+                    break
+            finalisolationzonecircuitscsv = downloadsfolderPath + '\\df_tempisolationzonedata' + '/' + file
+            finalisolationzonecircuits_actual = pd.read_csv(finalisolationzonecircuitscsv)
+
+            # Store devfile_isolationzonecircuits csv to dataframe
+            tempisozonedata = downloadsfolderPath + '\\devfile_isolationzonecircuits'
+            isolationzonecircuitscsv_expected = os.listdir(tempisozonedata)
+            for file1 in isolationzonecircuitscsv_expected:
+                if file1.endswith('csv'):
+                    break
+            finalisolationzonecircuitscsv_expected = downloadsfolderPath + '\\devfile_isolationzonecircuits' + '/' + file1
+            finalisolationzonecircuits_expected = pd.read_csv(finalisolationzonecircuitscsv_expected)
+
+            finalisolationzonecircuits_actual = finalisolationzonecircuits_actual.drop_duplicates(
+                subset=['circuitId', 'isolationzonename', 'tier2ohmiles', 'tier3ohmiles', 'totalohmiles',
+                        'tier2ugmiles', 'tier3ugmiles', 'totalugmiles', 'tier2ohpoles', 'tier3ohpoles', 'totalpoles'],
+                keep='first')
+            finalizcircuitsfolder = downloadsfolderPath + "\\Autofile_isolationzonecircuits"
+            deleteFolder(finalizcircuitsfolder)
+            create_folder(finalizcircuitsfolder)
+            finalisolationzonecircuits_actual.to_csv(finalizcircuitsfolder + '/finalisozone_actual.csv')
+
+            actualizcount = finalisolationzonecircuits_actual.shape[0]
+            log.info("Automation file Count of isolation zone records is: " + str(actualizcount))
+            expectedizcount = finalisolationzonecircuits_expected.shape[0]
+            log.info("Dev file Count of isolation zone records is: " + str(expectedizcount))
+            if str(actualizcount) == str(expectedizcount):
+                log.info(
+                    "Total isolation zone records count matched between Actual and Expected report and count is: " + str(
+                        actualizcount))
+                df1 = pd.merge(finalisolationzonecircuits_expected, finalisolationzonecircuits_actual,
+                               on=['circuitId', 'isolationzonename', 'tier2ohmiles', 'tier3ohmiles', 'totalohmiles',
+                                   'tier2ugmiles', 'tier3ugmiles', 'totalugmiles', 'tier2ohpoles', 'tier3ohpoles',
+                                   'totalpoles'],
+                               how='outer', indicator=True)
+                df1 = df1[df1['_merge'] != 'both']
+                if len(df1) == 0:
+                    log.info("All the isolationzones matched")
+                else:
+                    mismatchisolationzones = downloadsfolderPath + "\\mismatchisolationzones"
+                    deleteFolder(mismatchisolationzones)
+                    create_folder(mismatchisolationzones)
+                    df1.to_csv(mismatchisolationzones + '/mismatchisolationzones.csv')
+                    log.error(
+                        "All the isolationzones not matched and mismatched isolationzones are: " + str(df1))
+                    final_assert.append(False)
+            else:
+                log.error("Total isolationzones count not matched between Actual and Expected report")
+                final_assert.append(False)
+                df1 = pd.merge(finalisolationzonecircuits_expected, finalisolationzonecircuits_actual,
+                               on=['circuitId', 'isolationzonename', 'tier2ohmiles', 'tier3ohmiles', 'totalohmiles',
+                                   'tier2ugmiles', 'tier3ugmiles', 'totalugmiles', 'tier2ohpoles', 'tier3ohpoles',
+                                   'totalpoles'],
+                               how='outer', indicator=True)
+                df1 = df1[df1['_merge'] != 'both']
+                if len(df1) == 0:
+                    log.info("All the isolationzones matched")
+                else:
+                    mismatchisolationzones = downloadsfolderPath + "\\mismatchisolationzones"
+                    deleteFolder(mismatchisolationzones)
+                    create_folder(mismatchisolationzones)
+                    df1.to_csv(mismatchisolationzones + '/mismatchisolationzones.csv')
+                    log.error(
+                        "All the isolationzones not matched and mismatched isolationzones are: " + str(df1))
+                    final_assert.append(False)
         if var_execution_flag == 'fail':
             log.error("Execution failed: Errors found in execution!!")
             assert False
         log.info("----------------------------------------------------------------------------------------------")
         log.info("*************AUTOMATION EXECUTION COMPLETED*************")
+
+
