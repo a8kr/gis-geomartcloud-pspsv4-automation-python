888cebc5aaabad507ccc230be826cb3f4105aa77
K8MY <k8my@pge.com>
2021-03-22 15:43:31 -0700

    update 3-22

--- PSPSProject/src/Pages/EventPage.py
+++ PSPSProject/src/Pages/EventPage.py
@@ -1,6 +1,7 @@
 import os
 import time
 
+from PSPSProject.src.Pages.HomePage import HomePage
 from PSPSProject.src.Repository.uilocators import locators
 from PSPSProject.src.Repository.statictext import textMessage
 from PSPSProject.src.ReusableFunctions.uiactions import UI_Element_Actions
@@ -108,8 +109,92 @@ class EventPage:
         if uielements.iselementEnabled(locators.view_psps_scope_modal_back_button) == False:
             print("Validate that Back button disabled after click on Create time place")
 
-
         return var_timeplace
+
+    def circuit_fileupload(self, filepath, filename):
+        time.sleep(0.5)
+        try:
+            self.filepath = filepath
+            self.file = os.path.join(filepath, filename)
+            var_textFilename = self.driver.find_element_by_id(locators.uploadFile_id).get_attribute('value')
+            assert var_textFilename == ""
+            self.driver.find_element_by_id(locators.uploadFile_id).send_keys(self.file)
+            time.sleep(0.5)
+            message = self.driver.find_element_by_xpath(locators.dm_upload_validation).get_attribute(
+                'innerHTML')
+            return message
+        except:
+            assert False
+
+    def tp_validatefile(self, filepath, filename, ErrorMessage):
+        uielements = UI_Element_Actions(self.driver)
+        time.sleep(0.5)
+        try:
+            self.filepath = filepath
+            self.file = os.path.join(filepath, filename)
+            var_textFilename = self.driver.find_element_by_id(locators.uploadFile_id).get_attribute('value')
+            assert var_textFilename == ""
+            self.driver.find_element_by_id(locators.uploadFile_id).send_keys(self.file)
+            time.sleep(0.5)
+            message = self.driver.find_element_by_xpath(locators.new_timeplace_fileupload_error).get_attribute(
+                'innerHTML')
+            assert ErrorMessage == message
+            print("Error message displayed properly")
+            time.sleep(0.5)
+            return message
+        except:
+            assert False
+
+    def tp_validatefile_message(self, filepath, filename, ErrorMessage):
+        uielements = UI_Element_Actions(self.driver)
+        time.sleep(0.5)
+        try:
+            self.filepath = filepath
+            self.file = os.path.join(filepath, filename)
+            var_textFilename = self.driver.find_element_by_id(locators.uploadFile_id).get_attribute('value')
+            assert var_textFilename == ""
+            self.driver.find_element_by_id(locators.uploadFile_id).send_keys(self.file)
+            time.sleep(0.5)
+            message = self.driver.find_element_by_xpath(locators.new_timeplace_fileupload_error).get_attribute(
+                'innerHTML')
+            assert ErrorMessage == message
+            print("Error message displayed properly")
+            time.sleep(0.5)
+            return message
+        except:
+            assert False
+
+    def eventm_uploadfile(self, filename):
+        time.sleep(0.5)
+        uielements = UI_Element_Actions(self.driver)
+        homepage = HomePage(self.driver)
+        homepage.navigate_defaultManagement()
+        uielements.Click(locators.dm_uplaodfile)
+        var_totalcircuits = getCSVrowCount(testDatafolderPath, filename)
+        var_uploadsuccess = self.circuit_fileupload(testDatafolderPath, filename)
+        if var_uploadsuccess == 'Validation success':
+            print("Successfully uploaded circuits: " + var_uploadsuccess)
+        else:
+            print("Upload circuits failed: " + var_uploadsuccess)
+        self.driver.find_element_by_xpath(locators.dm_upload_btn).click()
+        time.sleep(0.5)
+        uielements.Click(locators.dm_save_btn)
+        while True:
+            try:
+                var_message = uielements.getValue(locators.dm_status_message)
+                if var_message in textMessage.upload_file_in_progress_message:
+                    continue
+                else:
+                    break
+            except:
+                break
+        var_message = uielements.getValue(locators.dm_status_message)
+        if var_message in textMessage.upload_file_successfully:
+            assert True, "Message 'Default devices data uploaded successfully' validation passed"
+        else:
+            assert False, "Message 'Default devices data uploaded successfully' validation failed"
+        return var_message
+
 
 
 
