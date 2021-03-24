e32ad9ff64b49155fe436e04a875b30367a23e61
K8MY <k8my@pge.com>
2021-03-24 11:52:13 -0700

    update 3-24

--- PSPSProject/src/Tests/test_psps_ui_eventmanagement_circuit_validation.py
+++ PSPSProject/src/Tests/test_psps_ui_eventmanagement_circuit_validation.py
@@ -45,7 +45,7 @@ class TestDefaultManagementNegative(BaseClass):
         final_assert = []
 
         log.info("----------------------------------------------------------------------------------------------")
-        log.info("Starting Validation")
+        log.info("-------------------------------- Starting Validation------------------------------------------")
         if var_os == "MAC-OS":
             homepage.SignOn()
             log.info("Successfully entered user id & password:")
@@ -60,55 +60,74 @@ class TestDefaultManagementNegative(BaseClass):
             log.error("Upload file button is not enabled by default")
             final_assert.append(False)
 
-        # Valid Circuit validation
-        log.info("Start Circuit error validation")
-        var_uploadFileName = "em_Valid_Circuits.csv"
-        var_error_message = "Validation success."
-        eventpage.tp_validatefile_message(testDatafolderPath, var_uploadFileName, var_error_message)
-        log.info("Validate File success message")
+        # # Valid Circuit validation
+        # log.info("Start Circuit error validation")
+        # var_uploadFileName = "em_Valid_Circuits.csv"
+        # var_error_message = "Validation success."
+        # eventpage.tp_validatefile_message(testDatafolderPath, var_uploadFileName, var_error_message)
+        # log.info("Validate File success message")
+        #
+        # var_Timestamp = getCurrentTime()
+        # var_tp_name_internal = "Auto_TP_" + var_Timestamp
+        # var_tp_name_external = "Ext_Auto_TP_" + var_Timestamp
+        # log.info("TP name: " + var_tp_name_internal)
+        #
+        # # Enter TP names
+        # uielements.setText(var_tp_name_internal, locators.new_timeplace_internal_name)
+        # uielements.setText(var_tp_name_external, locators.new_timeplace_external_name)
+        # log.info("Enter TP names")
+        #
+        # uielements.Click(locators.new_timeplace_create_button)
+        # log.info("Click on Create button")
+        # time.sleep(5)
+        # while True:
+        #     try:
+        #         var_status_message = uielements.getValue(locators.new_timeplace_validation_message)
+        #         if var_status_message in "Time place creation in progress.":
+        #             continue
+        #         else:
+        #             break
+        #     except:
+        #         break
+        # log.info("Wait for 'Time place creation in progress' completion " )
+        #
+        # if uielements.iselementDisplayed(locators.view_time_place_grid_header) == True:
+        #     log.info("TP created and navigated to View TP tab")
+        #
+        # # Get TP DB UID
+        # get_new_tp_db = queries.get_timeplace % var_tp_name_internal
+        # lst_table_details = queryresults_fetchone(get_new_tp_db)
+        # if lst_table_details:
+        #     log.info("TP record created in DB: uid is " + str(lst_table_details))
+        #
+        #  # Navigate to New TP tab
+        # uielements.Click(locators.new_time_place_new_tab)
 
-        var_Timestamp = getCurrentTime()
-        var_tp_name_internal = "Auto_TP_" + var_Timestamp
-        var_tp_name_external = "Ext_Auto_TP_" + var_Timestamp
-        log.info("TP name: " + var_tp_name_internal)
 
-        # Enter TP names
-        uielements.setText(var_tp_name_internal, locators.new_timeplace_internal_name)
-        uielements.setText(var_tp_name_external, locators.new_timeplace_external_name)
-        log.info("Enter TP names")
+        # # Clear form
+        # uielements.Click(locators.new_timeplace_clear_button)
+        # uielements.Click(locators.new_timeplace_clear_modal_yes_button)
+        #
+        # # Refresh form
+        # uielements.Click(locators.new_event_tab)
+        # uielements.Click(locators.new_time_place_new_tab)
 
-        uielements.Click(locators.new_timeplace_create_button)
-        log.info("Click on Create button")
-        time.sleep(5)
-        while True:
-            try:
-                var_status_message = uielements.getValue(locators.new_timeplace_validation_message)
-                if var_status_message in "Time place creation in progress.":
-                    continue
-                else:
-                    break
-            except:
-                break
-        log.info("Wait for 'Time place creation in progress' completion " )
-
-        if uielements.iselementDisplayed(locators.view_time_place_grid_header) == True:
-            log.info("TP created and navigated to View TP tab")
-
-        # Get TP DB UID
-        get_new_tp_db = queries.get_timeplace % var_tp_name_internal
-        lst_table_details = queryresults_fetchone(get_new_tp_db)
-        if lst_table_details:
-            log.info("TP record created in DB: uid is " + str(lst_table_details))
-
-        # Upload invalid files -- apart from csv
-        var_filename = readData(testDatafilePath, "Main", var_row, 9)
-        var_filename = var_filename.split(',')
-        nooffiles = len(var_filename)
-
-        uielements.Click(locators.new_time_place_new_tab)
 
         # File Errors Validations
-        log.info("Start files error validation")
+        # log.info("Start files error validation")
+        # var_uploadFileName = "em_Valid_Invalid_Circuits_logfiles.xlsx"
+        # var_error_message = "Invalid File! Allowed file is either a csv or a zip file."
+        # eventpage.tp_validatefile_message(testDatafolderPath, var_uploadFileName, var_error_message)
+        # log.info("Validate error message for zip and cvs files only")
+
+        # # Clear form
+        # uielements.Click(locators.new_timeplace_clear_button)
+        # uielements.Click(locators.new_timeplace_clear_modal_yes_button)
+        #
+        # # Refresh form
+        # uielements.Click(locators.new_event_tab)
+        # uielements.Click(locators.new_time_place_new_tab)
+
         for i in range(0, 24):
             log.info("Create cvs file from em_Valid_Invalid_Circuits_logfiles.xlsx file tab: " + str(i))
             var_row_num = i
